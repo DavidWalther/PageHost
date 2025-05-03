a0e34bec9012a24ebc6e4d5e3ed87e1cd1cf9e5b
DavidWalther <walthdav@gmail.com>
2025-05-03 19:04:43 +0200

    add Bearer to query requests

--- public/index.js
+++ public/index.js
@@ -94,6 +94,19 @@ function accessSessionStorage(key, action, value, callback) {
 }
 
 function fetchDatabase(eventpayload) {
+  let authData = accessSessionStorage('code_exchange_response', 'read');
+  authData = JSON.parse(authData);
+  let authBearer = authData.authenticationResult?.access?.access_token
+  let preparedHeaders = {
+              method: 'GET',
+              headers: {
+               'Content-Type': 'application/json',
+              }
+            };
+  if (authBearer) {
+    preparedHeaders.headers.Authorization = `Bearer ${authBearer}`
+  }
+
   return new Promise((resolve, reject) => {
     switch (eventpayload.object) {
       case 'story': {
@@ -131,9 +144,9 @@ function fetchDatabase(eventpayload) {
         let chapterId = eventpayload.id;
         let fetchPromises = [];
         fetchPromises.push(
-          fetch(`/data/query/chapter?id=${chapterId}`)
-            .then(chapterResponse => {
-              return chapterResponse.json()
+          fetch(`/data/query/chapter?id=${chapterId}`, preparedHeaders)
+          .then(chapterResponse => {
+            return chapterResponse.json()
         }));
 
         Promise.all(fetchPromises).then(response => {
@@ -144,7 +157,7 @@ function fetchDatabase(eventpayload) {
       }
       case 'paragraph': {
         let paragraphId = eventpayload.id;
-        fetch(`/data/query/paragraph?id=${paragraphId}`)
+        fetch(`/data/query/paragraph?id=${paragraphId}`, preparedHeaders)
         .then(paragraphResponse => paragraphResponse.json())
         .then(paragraph => {
           if(!paragraph || paragraph.length === 0) { reject('No paragraph found');}
