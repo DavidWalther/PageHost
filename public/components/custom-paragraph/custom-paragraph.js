ff1fa524be154b223cb2dc15c21ed60a181a44e1
DavidWalther <walthdav@gmail.com>
2025-10-29 07:34:29 +0100

    use back ground color for "Loading Paragraph"

--- public/components/custom-paragraph/custom-paragraph.js
+++ public/components/custom-paragraph/custom-paragraph.js
@@ -140,7 +140,7 @@ class CustomParagraph extends LitElement {
     } else if (this.noLoad) {
       // Show placeholder for lazy-loaded content with realistic size
       content = html`
-        <div class="slds-box slds-theme_shade" style="min-height: 80px; display: flex; align-items: center; justify-content: center;">
+        <div class="slds-box " style="min-height: 80px; display: flex; align-items: center; justify-content: center;">
           <div style="text-align: center;">
             <slds-spinner size="x-small"></slds-spinner>
             <p class="slds-text-color_weak slds-m-top_x-small">Loading paragraph...</p>
