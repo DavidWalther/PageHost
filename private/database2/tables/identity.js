ba0febc5e832332a93676dc331f9581188ca2264
DavidWalther <walthdav@gmail.com>
2025-12-29 23:07:56 +0100

    identity.js: create file for table metadata

---
+++ private/database2/tables/identity.js
@@ -0,0 +1,22 @@
+class TableIdentity {
+  constructor() {
+    this.tableName = 'identity';
+    this.tableFields = ['id', 'recordnumber', 'key', 'active', 'createddate'];
+    this.tableKeyPrefix = '000i';
+  }
+
+  getTableName() {
+    return () => this.tableName;
+  }
+
+  getTableFields() {
+    return () => [... this.tableFields];
+  }
+
+  getTableKeyPrefix() {
+    return () => this.tableKeyPrefix;
+  }
+}
+
+module.exports = { TableIdentity };
+
