7353e4e7a57e828e3fb5bd805a385d7aa0f35c96
DavidWalther <walthdav@gmail.com>
2026-01-10 17:59:58 +0100

    FaviconEndpointLogic: use database result to send file content

--- private/endpoints/*/FaviconEndpointLogic.js
+++ private/endpoints/*/FaviconEndpointLogic.js
@@ -23,6 +23,15 @@ class FaviconEndpointLogic extends EndpointLogic {
     return new Promise((resolve) => {
         dataFacade.getData(parameterObject).then(configuration => {
 
+        // Set the proper Content-Type based on the requested file
+        const requestedPath = this.requestObject.params[0];
+        if (requestedPath === 'favicon.svg') {
+          this.responseObject.setHeader('Content-Type', 'image/svg+xml');
+        } else if (requestedPath === 'favicon.ico') {
+          this.responseObject.setHeader('Content-Type', 'image/x-icon');
+        }
+
+        this.responseObject.send(configuration.icon.favicon);
         resolve();
       });
     });
