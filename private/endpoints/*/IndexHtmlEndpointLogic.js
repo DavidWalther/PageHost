8d9453fbb430ca97a1e68930b2b1da248b5f90c3
DavidWalther <walthdav@gmail.com>
2026-03-07 07:18:03 +0100

    IndexEndpoint: include new edit component in result

--- private/endpoints/*/IndexHtmlEndpointLogic.js
+++ private/endpoints/*/IndexHtmlEndpointLogic.js
@@ -41,6 +41,7 @@ class IndexHtmlEndpointLogic extends EndpointLogic {
       headerEntries.push('<script type="module" src="components/custom-story/custom-story.js"></script>');
       headerEntries.push('<script type="module" src="components/custom-publishing/custom-publishing.js"></script>');
       headerEntries.push('<script type="module" src="components/custom-login-module/custom-login-module.js"></script>');
+      headerEntries.push('<script type="module" src="components/custom-chapter-edit/custom-chapter-edit.js"></script>');
 
       headerEntries.push('<script type="module" src="slds-components/slds-toggle/toggle.js"></script>');
       headerEntries.push('<script type="module" src="slds-components/slds-input/slds-input.js"></script>');
