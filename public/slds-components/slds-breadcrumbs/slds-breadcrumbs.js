90e4800535f5cddb5629e0fb97be22f97bd93963
DavidWalther <walthdav@gmail.com>
2026-04-25 07:24:24 +0200

    Refactor spacing logic and update rendering in slds-breadcrumbs component

--- public/slds-components/slds-breadcrumbs/slds-breadcrumbs.js
+++ public/slds-components/slds-breadcrumbs/slds-breadcrumbs.js
@@ -6,7 +6,7 @@ class SldsBreadcrumbs extends LitElement {
     items: { type: Array },
     ariaLabel: { type: String, attribute: 'aria-label' },
     isCardContainer: { type: Boolean, attribute: 'card-container' },
-    size: { type: String, reflect: true } 
+    size: { type: String, reflect: true }
   };
 
   constructor() {
@@ -33,15 +33,42 @@ class SldsBreadcrumbs extends LitElement {
     return this.size === 'large';
   }
 
+  get spacingStart() {
+    if(this.isSizeLarge) {
+      return '1.75rem';
+    }
+    if(this.isSizeMedium) {
+      return '1rem';
+    }
+    return '.8rem';
+  }
+
+  get spacingEnd() {
+    if(this.isSizeLarge) {
+      return '1rem';
+    }
+    if(this.isSizeMedium) {
+      return '.75rem';
+    }
+
+    return '.5rem';
+  }
+
   render() {
+    const sizeClass = this.isSizeSmall ? 'slds-text-heading_small' : this.isSizeMedium ? 'slds-text-heading_medium' : this.isSizeLarge ? 'slds-text-heading_large' : '';
+    const spacingWidthStart = this.spacingStart;
+    const spacingWidthEnd = this.spacingEnd;
+    this.style.setProperty('--slds-c-breadcrumbs-spacing-inline-start', spacingWidthStart);
+    this.style.setProperty('--slds-c-breadcrumbs-spacing-inline-end', spacingWidthEnd);
+
     const content = html`
       <nav role="navigation" slot="${this.isCardContainer ? 'header' : ''}" aria-label="${this.ariaLabel}">
-        <ol class="slds-breadcrumb slds-list_horizontal slds-wrap">
+        <ol class="${sizeClass} slds-breadcrumb slds-list_horizontal slds-wrap">
           ${this.items.map((item, index) => this._renderItem(item, index))}
         </ol>
       </nav>
     `;
-    
+
     if(!this.isCardContainer) {
       return content;
     }
@@ -49,14 +76,13 @@ class SldsBreadcrumbs extends LitElement {
   }
 
   _renderItem(item, index) {
-    const sizeClass = this.isSizeSmall ? 'slds-text-heading_small' : this.isSizeMedium ? 'slds-text-heading_medium' : this.isSizeLarge ? 'slds-text-heading_large' : '';
 
     return html`
       <li class="slds-breadcrumb__item ">
         <a
           href="${item.href ?? nothing}"
           @click="${(event) => this._handleClick(event, item, index)}"
-        ><div class="${sizeClass}">${item.label}</div></a>
+        >${item.label}</a>
       </li>
     `;
   }
